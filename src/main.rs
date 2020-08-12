2909c6d20d0a3d07ef5ad6fc71d2ab47be65cd8c
Olly Legg <olly@51degrees.net>
2020-08-12 12:19:02 +0100

    Load config from ENV variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::error::Error;
+use std::process::exit;
+
+#[macro_use]
+extern crate envconfig_derive;
+#[macro_use]
+extern crate log;
+
+use envconfig::Envconfig;
+
+
+#[derive(Envconfig)]
+struct Config {
+    #[envconfig(from = "AWAIR_API_KEY")]
+    pub api_key: String,
+
+    #[envconfig(from = "AWAIR_DEVICE_TYPE")]
+    pub device_type: String,
+
+    #[envconfig(from = "AWAIR_DEVICE_ID")]
+    pub device_id: String,
+}
+
+fn run(config: Config) {}
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+
+    let result: Result<(), Box<dyn Error>> = Config::init()
+        .map(|config| {
+            run(config);
+        })
+        .map_err(|err| Box::new(err) as Box<dyn Error>);
+
+    match result {
+        Ok(_) => exit(0),
+        Err(err) => {
+            error!("{}", err);
+            exit(1)
+        }
+    }
 }
